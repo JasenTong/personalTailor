d70eb68a9d3568405d79ea4baa03bd5b879e6eb3
zfzhou <zzf15751774351@163.com>
2020-02-14 23:10:53 +0800

    support multi file upload

--- src/main/java/com/srdz/demo/controller/UploadController.java
+++ src/main/java/com/srdz/demo/controller/UploadController.java
@@ -11,6 +11,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 @Controller
 public class UploadController {
@@ -24,28 +25,33 @@ public class UploadController {
 
     @PostMapping("/upload")
     @ResponseBody
-    public String upload(@RequestParam("file") MultipartFile file) {
-        if(file.isEmpty()) {
-            return "Upload fail. Please select correct file";
-        }
+    public String upload(@RequestParam("files") List<MultipartFile> files) {
 
-        String fileName = file.getOriginalFilename();
         // TODO: need to upload server?
-        String filePath = "D:\\receive/";
-        File dest = new File(filePath + fileName);
+        String filePath = "/Users/zfzhou/test/";
 
-        if(!dest.getParentFile().exists()) {
-            dest.getParentFile().mkdirs();
-        }
+        for (int i = 0; i < files.size(); i++) {
+            MultipartFile file = files.get(i);
+            if(file.isEmpty()) {
+                return "上传第" + (i++) + "个文件失败";
+            }
 
-        try {
-            file.transferTo(dest);
-            LOGGER.info("Upload Success");
-            return "Upload Success";
-        } catch (IOException e) {
-            LOGGER.error(e.toString(), e);
+            String fileName = file.getOriginalFilename();
+            File dest = new File(filePath + fileName);
+
+            if(!dest.getParentFile().exists()) {
+                dest.getParentFile().mkdirs();
+            }
+
+            try {
+                file.transferTo(dest);
+                LOGGER.info("第" + (i + 1) + "个文件上传成功");
+            } catch (IOException e) {
+                LOGGER.error(e.toString(), e);
+                return "上传第" + (i++) + "个文件失败";
+            }
         }
-        return "Upload fail.";
+        return "上传成功";
     }
 
 }
